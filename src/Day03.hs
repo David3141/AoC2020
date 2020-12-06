068344597347e699dd37527b5594ed725a7ae437
David L <david.landry@veldrane.de>
2020-12-06 20:09:34 +0100

    Solve day 3 part 2

--- src/Day03.hs
+++ src/Day03.hs
@@ -11,14 +11,28 @@ import           Helpers                        ( readInts )
 import           Paths_advent_of_code
 
 part1 :: IO Int
-part1 = do
-    list <- input
-
-    return $ sum
-        [ 1 | (lane, index) <- zip list [0, 3 ..], (lane !! index) == '#' ]
+part1 = countTreeEncounters (3, 1) <$> input
 
 part2 :: IO Int
-part2 = return 0
+part2 = do
+    lanes <- input
+
+    return $ product
+        [ countTreeEncounters (1, 1) lanes
+        , countTreeEncounters (3, 1) lanes
+        , countTreeEncounters (5, 1) lanes
+        , countTreeEncounters (7, 1) lanes
+        , countTreeEncounters (1, 2) lanes
+        ]
 
 input :: IO [String]
 input = map cycle . lines <$> (readFile =<< getDataFileName "inputs/day03.txt")
+
+countTreeEncounters :: (Int, Int) -> [String] -> Int
+countTreeEncounters (right, down) lanes =
+    let numLanes = ceiling $ fromIntegral (length lanes) / fromIntegral down
+    in  length
+            [ 1
+            | (x, y) <- take numLanes $ zip [0, right ..] [0, down ..]
+            , ((lanes !! y) !! x) == '#'
+            ]
