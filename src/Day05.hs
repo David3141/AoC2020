6f9a9cfaded0145f4352d74b1be90547f1d8a386
David L <david.landry@veldrane.de>
2021-01-10 23:56:53 +0100

    Refactor day 5 by parsing as binary

--- src/Day05.hs
+++ src/Day05.hs
@@ -13,17 +13,14 @@ part2 = missingSeatId . map seatId <$> readPasses
 readPasses :: IO [String]
 readPasses = lines <$> (readFile =<< getDataFileName "inputs/day05.txt")
 
-parseRow :: String -> Int
-parseRow = binaryPartition (0, 127) ('F', 'B')
-
-parseColumn :: String -> Int
-parseColumn = binaryPartition (0, 7) ('L', 'R')
-
 seatId :: String -> Int
-seatId str = row * 8 + column
+seatId = parseAsBinary
+
+parseAsBinary :: String -> Int
+parseAsBinary =
+    foldl (\acc c -> if countsAsOne c then acc * 2 + 1 else acc * 2) 0
     where
-        row = parseRow . take 7 $str
-        column = parseColumn . drop 7 $ str
+        countsAsOne c = c `elem` "BR"
 
 -- We know that the searched seat has direct neighbors. So to find it,
 -- we need to sort all IDs and find an occurence of x followed by x + 2.
@@ -34,13 +31,3 @@ missingSeatId seatIds = missingSeatId' (sort seatIds)
     missingSeatId' (x:y:rest) = if y - x == 2
         then x + 1
         else missingSeatId' (y:rest)
-
-binaryPartition :: (Int, Int) -> (Char, Char) -> String -> Int
-binaryPartition (min, max) (lowerChar, upperChar) str =
-    binaryPartition' str (min, max)
-    where
-        binaryPartition' :: String -> (Int, Int) -> Int
-        binaryPartition' [] (min, max) = min
-        binaryPartition' (c:rest) (min, max) = if c == lowerChar
-            then binaryPartition' rest (min, max - (max - min) `div` 2 - 1)
-            else binaryPartition' rest (min + (max - min) `div` 2 + 1, max)
