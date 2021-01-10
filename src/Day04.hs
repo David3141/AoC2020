b4cda56e94ab2b5d9862f37a84193afcabd8a79a
David L <david.landry@veldrane.de>
2021-01-10 18:58:09 +0100

    Rewrite day 4 part 1 using applicatives
    With help of https://www.youtube.com/watch?v=3ZAq-2_SCcI
    (Video of Alan Malloy solving Day 4 in Haskell)

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,34 +1,42 @@
 module Day04 (part1, part2) where
 
+import Data.Maybe (fromMaybe)
+import Data.Char (isAlphaNum)
 import Data.List (delete, groupBy)
 import Data.List.Split (splitOn)
 import Paths_advent_of_code
+import Text.Regex.Applicative
+import qualified Data.Map.Strict as M
+
+type Passport = M.Map String String
 
 part1 :: IO Int
-part1 = countBy hasAllRequiredTokens <$> readPassports
+part1 = countBy hasAllRequiredEntries <$> readPassports
+    where
+        hasAllRequiredEntries :: Passport -> Bool
+        hasAllRequiredEntries passport =
+            all (`M.member` passport) requiredEntry
+
+        requiredEntry = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
 part2 :: IO Int
-part2 = return 0
+part2 = return 4
 
-readPassports :: IO [[String]]
+readPassports :: IO [Passport]
 readPassports =
-  map (concatMap words) . groupBy (\c1 c2 -> c2 /= "") . lines
+    map parsePassport . splitOn "\n\n"
     <$> (readFile =<< getDataFileName "inputs/day04.txt")
 
-hasAllRequiredTokens :: [String] -> Bool
-hasAllRequiredTokens = null . filterRequiredTokens requiredTokens
-  where
-    requiredTokens = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-
-filterRequiredTokens :: [String] -> [String] -> [String]
-filterRequiredTokens remainingTokens [] = remainingTokens
-filterRequiredTokens [] _ = []
-filterRequiredTokens remainingTokens (pair : pairs) =
-  filterRequiredTokens newRemainingTokens pairs
-  where
-    currentToken = head . splitOn ":" $ pair
-
-    newRemainingTokens = delete currentToken remainingTokens
+parsePassport :: String -> Passport
+parsePassport = M.fromList . fromMaybe [] . match passport
+    where
+        passport = many entry           :: RE Char [(String, String)]
+        entry = (,) <$> key <*> value   :: RE Char (String, String)
+        key = whitespace *> many (psym (/= ':'))
+        value = sym ':' *> nonWhitespace <* whitespace
+        whitespace = many (psym isWhitespace)
+        nonWhitespace = many (psym (not . isWhitespace))
+        isWhitespace = (`elem` " \n\t")
 
 countBy :: (a -> Bool) -> [a] -> Int
 countBy cond [] = 0
