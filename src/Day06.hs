af727ca4d63033812e9b1d91e0502d037ebfbbd9
David L <david.landry@veldrane.de>
2021-01-11 18:33:58 +0100

    Solve day 6 part 2

--- src/Day06.hs
+++ src/Day06.hs
@@ -6,13 +6,24 @@ import Data.List.Split (splitOn)
 import Paths_advent_of_code
 
 part1 :: IO Int
-part1 = sum . map (length . group . sort) <$> readAnswers
+part1 = sum . map countAllUniqueLetters <$> readAnswers
 
 part2 :: IO Int
-part2 = return 6
+part2 = sum . map countLettersAppearingInEachLine <$> readAnswers
+
 
 readAnswers :: IO [String]
-readAnswers = map removeWhitespace . splitOn "\n\n" <$> (readFile =<< getDataFileName "inputs/day06.txt")
+readAnswers = splitOn "\n\n" <$> (readFile =<< getDataFileName "inputs/day06.txt")
 
 removeWhitespace :: String -> String
 removeWhitespace = filter (not . (`elem` " \n"))
+
+countAllUniqueLetters :: String -> Int
+countAllUniqueLetters = length . group . sort . removeWhitespace
+
+countLettersAppearingInEachLine :: String -> Int
+countLettersAppearingInEachLine answers =
+    length . filter (== numPersons) . countEachLetter $ answers
+    where
+        countEachLetter = map length . group . sort . removeWhitespace
+        numPersons = length . lines $ answers
