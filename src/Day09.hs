8e94b859ed7b0acc49c52dc33bd2b7f6cf53ac6d
David L <david.landry@veldrane.de>
2021-01-15 00:00:09 +0100

    Solve day 9 part 2

--- src/Day09.hs
+++ src/Day09.hs
@@ -6,19 +6,32 @@ part1 :: IO Int
 part1 = firstInvalidNum <$> input
 
 part2 :: IO Int
-part2 = return 9
+part2 = do
+  numbers <- input
+
+  let invalidNum = firstInvalidNum numbers
+  let sequence   = findSequenceSummingToNum invalidNum numbers
+
+  return $ sum [minimum sequence, maximum sequence]
 
 input :: IO [Int]
 input = readInts "inputs/day09.txt"
 
 firstInvalidNum :: [Int] -> Int
-firstInvalidNum (x:rest)
-  | hasInvalidNum = candidate
-  | otherwise = firstInvalidNum rest
-  where
-  (preamble, [candidate]) = splitAt 25 $ take 26 (x:rest)
+firstInvalidNum (x : rest) | hasInvalidNum = candidate
+                           | otherwise     = firstInvalidNum rest
+ where
+  (preamble, [candidate]) = splitAt 25 $ take 26 (x : rest)
   matchingPairs = filter (\(x, y) -> x + y == candidate) (pairs preamble)
   hasInvalidNum = null matchingPairs
 
+findSequenceSummingToNum :: Int -> [Int] -> [Int]
+findSequenceSummingToNum invalidNum (x : rest)
+  | not (null candidates) = head candidates
+  | otherwise             = findSequenceSummingToNum invalidNum rest
+ where
+  sequences  = map (\count -> take count (x : rest)) [2 .. length (x : rest)]
+  candidates = filter ((== invalidNum) . sum) sequences
+
 pairs :: Eq a => [a] -> [(a, a)]
-pairs list = [(x, y) | x <- list, y <- list, x /= y]
+pairs list = [ (x, y) | x <- list, y <- list, x /= y ]
