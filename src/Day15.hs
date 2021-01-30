2e806133d2fbeb0d32e83cf13a9b8dbf7d79e7ce
David L <david.landry@veldrane.de>
2021-01-31 00:28:03 +0100

    Solve day 15 part 2
    This also improves the runtime, though I don't understand why. The
    following changes were necessary to get to a reasonable runtime in
    part 2:
    
    `... $ play`  (old, play was `iterate next initialGame`)
    =>
    `... . iterate next $ initialGame`  (new)
    
    and
    
    `(!! (n - length inputs))`  (old, to access the correct GameState)
    =>
    `head . filter ((== n) . prevTurn)`  (new)

--- src/Day15.hs
+++ src/Day15.hs
@@ -5,7 +5,11 @@ import qualified Data.IntMap.Strict as M
 
 
 type Memory = M.IntMap Int
-data GameState = GameState Memory Int Int deriving Show
+data GameState = GameState {
+  memory :: Memory,
+  prevNum :: Int,
+  prevTurn :: Int
+} deriving Show
 
 
 input :: [Int]
@@ -13,32 +17,29 @@ input = [1, 12, 0, 20, 8, 16]
 
 
 part1 :: IO Int
-part1 = do
-  let (GameState _ prevNum _) = (!! (2020 - length input)) play
-
-  return prevNum
+part1 = return $ nthNum 2020
 
 
-play :: [GameState]
-play = iterate next (GameState initialMemory prevNum turn)
-  where
-    prevNum = last input
-    turn = length input + 1
-    initialMemory = foldl
-      (\acc (turn, num) -> M.alter (const $ Just turn) num acc)
-      M.empty
-      (zip [1 ..] input)
+part2 :: IO Int
+part2 = return $ nthNum 30000000
+
+
+nthNum :: Int -> Int
+nthNum n =
+  prevNum . head . filter ((== n) . prevTurn) . iterate next $ initialGame
+
+
+initialGame :: GameState
+initialGame = GameState { memory   = M.fromList (zip (init input) [1 ..])
+                        , prevNum  = last input
+                        , prevTurn = length input
+                        }
 
 
 next :: GameState -> GameState
 next (GameState memory prevNum turn) = GameState newMemory nextNum (turn + 1)
-  where
-    newMemory = M.alter (const $ Just (turn - 1)) prevNum memory
-    nextNum = case M.lookup prevNum memory of
-        Just someTurn -> (turn - 1) - someTurn
-        Nothing -> 0
-
-
-part2 :: IO Int
-part2 = return 4
-
+ where
+  newMemory = M.insert prevNum turn memory
+  nextNum   = case M.lookup prevNum memory of
+    Just someTurn -> turn - someTurn
+    Nothing       -> 0
