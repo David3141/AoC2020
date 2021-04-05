bd1ab245c3b849d465d22387f4e50f9eed3de0be
David L <david.landry@veldrane.de>
2021-04-05 22:36:31 +0200

    Solve day 16 part 2

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,14 +1,21 @@
 {-# LANGUAGE ViewPatterns #-}
 
-module Day16 (part1, part2)
-where
+module Day16
+  ( part1
+  , part2
+  ) where
 
-import Data.List.Split (splitOn)
+import           Data.List                      ( isPrefixOf
+                                                , permutations
+                                                , transpose
+                                                )
+import           Data.List.Split                ( splitOn )
 
-import Paths_advent_of_code ( getDataFileName )
+import           Paths_advent_of_code           ( getDataFileName )
 
-type Rule = (String, (Int, Int), (Int, Int))
+type Rule = (String, Int -> Bool)
 type Ticket = [Int]
+
 
 part1 :: IO Int
 part1 = do
@@ -18,7 +25,20 @@ part1 = do
 
 
 part2 :: IO Int
-part2 = return 16
+part2 = do
+  (rules, myTicket, tickets) <- input
+
+  let validTickets = filter (all (numMatchesAnyRule rules)) tickets
+
+  let getMatchingRuleNames nums =
+        map fst . filter (\(_, matches) -> all matches nums) $ rules
+  let matchingRules = map getMatchingRuleNames (transpose validTickets)
+  let validRules =
+        concat $ repeatUntilNoChange removeSingletonDups matchingRules
+
+  return $ product . map snd . filter (isPrefixOf "departure" . fst) $ zip
+    validRules
+    myTicket
 
 
 input :: IO ([Rule], Ticket, [Ticket])
@@ -34,19 +54,33 @@ input = do
 
 
 parseRule :: String -> Rule
-parseRule rule = (name, (min1, max1), (min2, max2))
-  where
-    [name, ranges] = splitOn ": " rule
-    [range1, range2] = splitOn " or " ranges
-    [read -> min1, read -> max1] = splitOn "-" range1
-    [read -> min2, read -> max2] = splitOn "-" range2
-
-
-numMatchesRule :: Rule -> Int -> Bool
-numMatchesRule (_, (min1, max1), (min2, max2)) num =
-  (min1 <= num && num <= max1) || (min2 <= num && num <= max2)
+parseRule rule = (name, numMatchesRule)
+ where
+  [name        , ranges      ] = splitOn ": " rule
+  [range1      , range2      ] = splitOn " or " ranges
+  [read -> min1, read -> max1] = splitOn "-" range1
+  [read -> min2, read -> max2] = splitOn "-" range2
+  numMatchesRule num =
+    (min1 <= num && num <= max1) || (min2 <= num && num <= max2)
 
 
 numMatchesAnyRule :: [Rule] -> Int -> Bool
-numMatchesAnyRule rules num =
-  any (`numMatchesRule` num) rules
+numMatchesAnyRule rules num = any (`snd` num) rules
+
+
+-- Example: [["a"], ["a", "b", "c"]]
+--       => [["a"], ["b", "c"]]
+removeSingletonDups :: [[String]] -> [[String]]
+removeSingletonDups rules = newRules
+ where
+  newRules   = map removeDups rules
+  singletons = concat . filter ((== 1) . length) $ rules
+
+  removeDups :: [String] -> [String]
+  removeDups [rule] = [rule]
+  removeDups rules  = filter (`notElem` singletons) rules
+
+
+repeatUntilNoChange :: Eq a => (a -> a) -> a -> a
+repeatUntilNoChange f x =
+  let x' = f x in if x' == x then x else repeatUntilNoChange f x'
