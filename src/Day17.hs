802b7c2c9c932e9a4b31ceb7830479f4a617ce51
David L <david.landry@veldrane.de>
2021-04-07 22:11:19 +0200

    Solve day 17 part 2

--- src/Day17.hs
+++ src/Day17.hs
@@ -13,64 +13,119 @@ import           Paths_advent_of_code           ( getDataFileName )
 import           Helpers                        ( readInts )
 
 
-type Coord = (Int, Int, Int)
 data State = Active | Inactive deriving (Show, Eq)
-type Cubes = M.Map Coord State
+type Coord3 = (Int, Int, Int)
+type Coord4 = (Int, Int, Int, Int)
+
+type Cubes3 = M.Map Coord3 State
+
+type Cubes4 = M.Map Coord4 State
 
 
 part1 :: IO Int
 part1 = length . M.filter (== Active) . (!! 6) . iterate tick <$> input
+ where
+  input :: IO Cubes3
+  input = do
+    strings <- lines <$> (readFile =<< getDataFileName "inputs/day17.txt")
+
+    let indexedStrings = zip [0 ..] (map (zip [0 ..]) strings)
+    let toState char = if char == '#' then Active else Inactive
+    let x = concatMap
+          (\(y, xs) -> map (\(x, char) -> ((x, y, 0), toState char)) xs)
+          indexedStrings
+
+    return $ M.fromList x
+
+
+  tick :: Cubes3 -> Cubes3
+  tick cubes = M.mapWithKey (transformCube cubes) . initNeighbors $ cubes
+
+
+  transformCube :: Cubes3 -> Coord3 -> State -> State
+  transformCube cubes coord state = newState
+   where
+    newState
+      | state == Active && activeNeighborCount `notElem` [2, 3] = Inactive
+      | state == Inactive && activeNeighborCount == 3 = Active
+      | otherwise = state
+    activeNeighborCount = length . M.filter (== Active) $ neighborhood
+    neighborhood        = neighbors cubes coord
+
+
+  neighbors :: Cubes3 -> Coord3 -> Cubes3
+  neighbors cubes coord = M.fromList $ map
+    (\coord -> (coord, M.findWithDefault Inactive coord cubes))
+    (neighboringIndices coord)
+
+   where
+    neighboringIndices :: Coord3 -> [Coord3]
+    neighboringIndices (x, y, z) =
+      [ (a, b, c)
+      | a <- [x - 1 .. x + 1]
+      , b <- [y - 1 .. y + 1]
+      , c <- [z - 1 .. z + 1]
+      , (a, b, c) /= (x, y, z)
+      ]
+
+
+  initNeighbors :: Cubes3 -> Cubes3
+  initNeighbors cubes = M.foldlWithKey'
+    (\acc coord _ -> M.union (neighbors cubes coord) acc)
+    cubes
+    cubes
 
 
 part2 :: IO Int
-part2 = return 17
-
-
-input :: IO Cubes
-input = do
-  strings <- lines <$> (readFile =<< getDataFileName "inputs/day17.txt")
-
-  let indexedStrings = zip [0 ..] (map (zip [0 ..]) strings)
-  let toState char = if char == '#' then Active else Inactive
-  let x = concatMap
-        (\(y, xs) -> map (\(x, char) -> ((x, y, 0), toState char)) xs)
-        indexedStrings
-
-  return $ M.fromList x
-
-
-tick :: Cubes -> Cubes
-tick cubes = M.mapWithKey (transformCube cubes) . initNeighbors $ cubes
-
-
-transformCube :: Cubes -> Coord -> State -> State
-transformCube cubes coord state = newState
+part2 = length . M.filter (== Active) . (!! 6) . iterate tick <$> input
  where
-  newState | state == Active && activeNeighborCount `notElem` [2, 3] = Inactive
-           | state == Inactive && activeNeighborCount == 3 = Active
-           | otherwise = state
-  activeNeighborCount = length . M.filter (== Active) $ neighborhood
-  neighborhood        = neighbors cubes coord
+  input :: IO Cubes4
+  input = do
+    strings <- lines <$> (readFile =<< getDataFileName "inputs/day17.txt")
+
+    let indexedStrings = zip [0 ..] (map (zip [0 ..]) strings)
+    let toState char = if char == '#' then Active else Inactive
+    let x = concatMap
+          (\(y, xs) -> map (\(x, char) -> ((x, y, 0, 0), toState char)) xs)
+          indexedStrings
+
+    return $ M.fromList x
 
 
-neighbors :: Cubes -> Coord -> Cubes
-neighbors cubes coord = M.fromList $ map
-  (\coord -> (coord, M.findWithDefault Inactive coord cubes))
-  (neighboringIndices coord)
-
- where
-  neighboringIndices :: Coord -> [Coord]
-  neighboringIndices (x, y, z) =
-    [ (a, b, c)
-    | a <- [x - 1 .. x + 1]
-    , b <- [y - 1 .. y + 1]
-    , c <- [z - 1 .. z + 1]
-    , (a, b, c) /= (x, y, z)
-    ]
+  tick :: Cubes4 -> Cubes4
+  tick cubes = M.mapWithKey (transformCube cubes) . initNeighbors $ cubes
 
 
-initNeighbors :: Cubes -> Cubes
-initNeighbors cubes = M.foldlWithKey'
-  (\acc coord _ -> M.union (neighbors cubes coord) acc)
-  cubes
-  cubes
+  transformCube :: Cubes4 -> Coord4 -> State -> State
+  transformCube cubes coord state = newState
+   where
+    newState
+      | state == Active && activeNeighborCount `notElem` [2, 3] = Inactive
+      | state == Inactive && activeNeighborCount == 3 = Active
+      | otherwise = state
+    activeNeighborCount = length . M.filter (== Active) $ neighborhood
+    neighborhood        = neighbors cubes coord
+
+
+  neighbors :: Cubes4 -> Coord4 -> Cubes4
+  neighbors cubes coord = M.fromList $ map
+    (\coord -> (coord, M.findWithDefault Inactive coord cubes))
+    (neighboringIndices coord)
+
+   where
+    neighboringIndices :: Coord4 -> [Coord4]
+    neighboringIndices (x, y, z, w) =
+      [ (a, b, c, d)
+      | a <- [x - 1 .. x + 1]
+      , b <- [y - 1 .. y + 1]
+      , c <- [z - 1 .. z + 1]
+      , d <- [w - 1 .. w + 1]
+      , (a, b, c, d) /= (x, y, z, w)
+      ]
+
+
+  initNeighbors :: Cubes4 -> Cubes4
+  initNeighbors cubes = M.foldlWithKey'
+    (\acc coord _ -> M.union (neighbors cubes coord) acc)
+    cubes
+    cubes
