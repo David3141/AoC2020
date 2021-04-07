4e49b027f057e8ee5ca946a0d63563df5714da5f
David L <david.landry@veldrane.de>
2021-04-07 23:41:21 +0200

    Refactor day 17 using a type class

--- src/Day17.hs
+++ src/Day17.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -Wno-deferred-type-errors #-}
+{-# LANGUAGE FlexibleInstances #-}
 module Day17
   ( part1
   , part2
@@ -14,89 +14,75 @@ import           Helpers                        ( readInts )
 
 
 data State = Active | Inactive deriving (Show, Eq)
-type Coord3 = (Int, Int, Int)
-type Coord4 = (Int, Int, Int, Int)
+type Coords3 = (Int, Int, Int)
+type Coords4 = (Int, Int, Int, Int)
 
-type Cubes3 = M.Map Coord3 State
 
-type Cubes4 = M.Map Coord4 State
+class (Ord a, Eq a) => Coords a where
+  initialCoordinates :: Int -> Int -> a
+  neighboringIndices :: a -> [a]
+
+
+instance Coords Coords3 where
+  initialCoordinates x y = (x, y, 0)
+  neighboringIndices (x, y, z) =
+    [ (a, b, c)
+    | a <- [x - 1 .. x + 1]
+    , b <- [y - 1 .. y + 1]
+    , c <- [z - 1 .. z + 1]
+    , (a, b, c) /= (x, y, z)
+    ]
+
+
+instance Coords Coords4 where
+  initialCoordinates x y = (x, y, 0, 0)
+  neighboringIndices (x, y, z, w) =
+    [ (a, b, c, d)
+    | a <- [x - 1 .. x + 1]
+    , b <- [y - 1 .. y + 1]
+    , c <- [z - 1 .. z + 1]
+    , d <- [w - 1 .. w + 1]
+    , (a, b, c, d) /= (x, y, z, w)
+    ]
 
 
 part1 :: IO Int
-part1 = length . M.filter (== Active) . (!! 6) . iterate tick <$> input
- where
-  input :: IO Cubes3
-  input = do
-    strings <- lines <$> (readFile =<< getDataFileName "inputs/day17.txt")
-
-    let indexedStrings = zip [0 ..] (map (zip [0 ..]) strings)
-    let toState char = if char == '#' then Active else Inactive
-    let x = concatMap
-          (\(y, xs) -> map (\(x, char) -> ((x, y, 0), toState char)) xs)
-          indexedStrings
-
-    return $ M.fromList x
-
-
-  tick :: Cubes3 -> Cubes3
-  tick cubes = M.mapWithKey (transformCube cubes) . initNeighbors $ cubes
-
-
-  transformCube :: Cubes3 -> Coord3 -> State -> State
-  transformCube cubes coord state = newState
-   where
-    newState
-      | state == Active && activeNeighborCount `notElem` [2, 3] = Inactive
-      | state == Inactive && activeNeighborCount == 3 = Active
-      | otherwise = state
-    activeNeighborCount = length . M.filter (== Active) $ neighborhood
-    neighborhood        = neighbors cubes coord
-
-
-  neighbors :: Cubes3 -> Coord3 -> Cubes3
-  neighbors cubes coord = M.fromList $ map
-    (\coord -> (coord, M.findWithDefault Inactive coord cubes))
-    (neighboringIndices coord)
-
-   where
-    neighboringIndices :: Coord3 -> [Coord3]
-    neighboringIndices (x, y, z) =
-      [ (a, b, c)
-      | a <- [x - 1 .. x + 1]
-      , b <- [y - 1 .. y + 1]
-      , c <- [z - 1 .. z + 1]
-      , (a, b, c) /= (x, y, z)
-      ]
-
-
-  initNeighbors :: Cubes3 -> Cubes3
-  initNeighbors cubes = M.foldlWithKey'
-    (\acc coord _ -> M.union (neighbors cubes coord) acc)
-    cubes
-    cubes
+part1 = countActiveAfter6Iterations <$> (input :: IO (M.Map Coords3 State))
 
 
 part2 :: IO Int
-part2 = length . M.filter (== Active) . (!! 6) . iterate tick <$> input
+part2 = countActiveAfter6Iterations <$> (input :: IO (M.Map Coords4 State))
+
+
+countActiveAfter6Iterations :: (Coords a) => M.Map a State -> Int
+countActiveAfter6Iterations =
+  length . M.filter (== Active) . (!! 6) . iterate tick
+
+
+readLines :: IO [String]
+readLines = lines <$> (readFile =<< getDataFileName "inputs/day17.txt")
+
+
+input :: (Coords a) => IO (M.Map a State)
+input = readCubes <$> readLines
  where
-  input :: IO Cubes4
-  input = do
-    strings <- lines <$> (readFile =<< getDataFileName "inputs/day17.txt")
+  readCubes :: Coords a => [String] -> M.Map a State
+  readCubes strings = M.fromList keyValueList
+   where
+    indexedStrings = zip [0 ..] (map (zip [0 ..]) strings)
+    keyValueList   = concatMap
+      (\(y, xs) -> map (\(x, char) -> (initialCoordinates x y, toState char)) xs
+      )
+      indexedStrings
 
-    let indexedStrings = zip [0 ..] (map (zip [0 ..]) strings)
-    let toState char = if char == '#' then Active else Inactive
-    let x = concatMap
-          (\(y, xs) -> map (\(x, char) -> ((x, y, 0, 0), toState char)) xs)
-          indexedStrings
-
-    return $ M.fromList x
-
-
-  tick :: Cubes4 -> Cubes4
-  tick cubes = M.mapWithKey (transformCube cubes) . initNeighbors $ cubes
+  toState :: Char -> State
+  toState char = if char == '#' then Active else Inactive
 
 
-  transformCube :: Cubes4 -> Coord4 -> State -> State
+tick :: (Coords a) => M.Map a State -> M.Map a State
+tick cubes = M.mapWithKey (transformCube cubes) . initNeighbors $ cubes
+ where
+  transformCube :: (Coords a) => M.Map a State -> a -> State -> State
   transformCube cubes coord state = newState
    where
     newState
@@ -106,26 +92,13 @@ part2 = length . M.filter (== Active) . (!! 6) . iterate tick <$> input
     activeNeighborCount = length . M.filter (== Active) $ neighborhood
     neighborhood        = neighbors cubes coord
 
-
-  neighbors :: Cubes4 -> Coord4 -> Cubes4
-  neighbors cubes coord = M.fromList $ map
-    (\coord -> (coord, M.findWithDefault Inactive coord cubes))
-    (neighboringIndices coord)
-
-   where
-    neighboringIndices :: Coord4 -> [Coord4]
-    neighboringIndices (x, y, z, w) =
-      [ (a, b, c, d)
-      | a <- [x - 1 .. x + 1]
-      , b <- [y - 1 .. y + 1]
-      , c <- [z - 1 .. z + 1]
-      , d <- [w - 1 .. w + 1]
-      , (a, b, c, d) /= (x, y, z, w)
-      ]
-
-
-  initNeighbors :: Cubes4 -> Cubes4
+  initNeighbors :: (Coords a) => M.Map a State -> M.Map a State
   initNeighbors cubes = M.foldlWithKey'
     (\acc coord _ -> M.union (neighbors cubes coord) acc)
     cubes
     cubes
+
+  neighbors :: (Coords a) => M.Map a State -> a -> M.Map a State
+  neighbors cubes coord = M.fromList $ map
+    (\coord -> (coord, M.findWithDefault Inactive coord cubes))
+    (neighboringIndices coord)
